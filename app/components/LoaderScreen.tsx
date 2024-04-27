8a85a49c3bbae46de41dc0835b278c906cc24489
lizozom <lizka.k@gmail.com>
2024-04-27 19:27:24 +0300

    this can take a minute

--- app/components/LoaderScreen.tsx
+++ app/components/LoaderScreen.tsx
@@ -6,10 +6,13 @@ export interface LoadingScreenProps {
 export function LoadingScreen(props: LoadingScreenProps) {
   
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen relative">
         <div className="flex my-[50%] flex-col gap-8 w-full">
             <span className="text-2xl text-center items-center">working on making<br/>you less stupid...</span>
             <div className="dropdown-button h-16  animate-width"></div>
+            <span className="text-xl w-full text-center absolute bottom-[35px] left-[50%] -translate-x-[50%]">
+              this can take a minute or two
+            </span>
         </div>
     </div>
 
