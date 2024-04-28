0eb7790f11547f430f328683a414036b16ef3e70
lizozom <lizka.k@gmail.com>
2024-04-28 16:20:16 +0300

    font

--- app/components/LoaderScreen.tsx
+++ app/components/LoaderScreen.tsx
@@ -10,7 +10,7 @@ export function LoadingScreen(props: LoadingScreenProps) {
         <div className="flex my-[50%] flex-col gap-8 w-full">
             <span className="text-2xl text-center items-center">working on making<br/>you less stupid...</span>
             <div className="dropdown-button h-16  animate-width"></div>
-            <span className="text-m w-full text-center absolute bottom-[35px] left-[50%] -translate-x-[50%]">
+            <span className="text-main w-full text-center absolute bottom-[35px] left-[50%] -translate-x-[50%]">
               this can take a minute or two
             </span>
         </div>
