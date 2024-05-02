8d7dd76afe73c2bde10f8e5bd5c7e935392a04e6
lizozom <lizka.k@gmail.com>
2024-05-02 14:04:08 +0300

    topics

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,22 @@ import { CommuteApp } from "./components/CommuteApp";
 
 const topics = [
   'the history of toothbrushes',
-  'how frogs make love',
-  'how coffee came to europe',
+  // 'how frogs make love',
+  'how did coffee come to europe',
   'what is a unicorn',
   'why flowers bloom in spring',
   'where do squirrels sleep',
   'the way to make silicone',
   'why are some people left handed',
   'what are the biggest creatures in the ocean',
-  'where did people poo in ancient times',
+  // 'where did people poo in ancient times',
   'why some european countries still have monarchs',
   'the paint of the golden gate bridge',
   'the history of the london tube',
   'how mcdonald\'s works',
   'who invented peanut butter',
   'star ratings of hotels',
-  'color blind animals',
+  // 'color blind animals',
   'the story of fabergé eggs'
 ]
 
