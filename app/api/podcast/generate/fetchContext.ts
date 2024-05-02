35325f143bf8d2bcd71f8da3f44f3ce2c75cb320
lizozom <lizka.k@gmail.com>
2024-05-02 15:00:21 +0300

    context

--- app/api/podcast/generate/fetchContext.ts
+++ app/api/podcast/generate/fetchContext.ts
@@ -11,7 +11,7 @@ const getWikiContent = async (topic: string) => {
 
 export const fetchContext = async (topic: string) => {
     const relavantWikiTopicsPrompt = `
-        Give me a list of relevant wikipedia page for the topic "${topic}". Return the response as a JSON list.
+        Give me a list of AT MOST 3 relevant wikipedia page for the topic "${topic}". Return the response as a JSON list.
         The list should only contain the topics, for example: ["Topic A", "Topic B", "Topic C"].
         IMPORTANT! Return ONLY a JSON object. Don't add quotes or comments around it.
     `;
