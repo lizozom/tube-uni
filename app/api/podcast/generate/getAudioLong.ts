91f0efece8ca5ba7b106638b4fbb458779c6aa08
lizozom <lizka.k@gmail.com>
2024-06-08 16:56:18 +0300

    use openai tts

--- app/api/podcast/generate/getAudioLong.ts
+++ app/api/podcast/generate/getAudioLong.ts
@@ -2,82 +2,114 @@
 import { Storage } from '@google-cloud/storage';
 import { v1beta1 } from '@google-cloud/text-to-speech';
 import { credentials } from './credentials';
+import OpenAI from "openai";
+
+const apiKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({
+  apiKey,
+});
 
 const ttsLongClient = new v1beta1.TextToSpeechLongAudioSynthesizeClient({
-    credentials
-  });
+  credentials
+});
 
 const storageClient = new Storage({
-    credentials
-  });
+  credentials
+});
+
+const GS_PATH = 'gs://tube-uni-podcasts/podcasts/';
 
 const checkFileExists = async (fileName: string) => {
-    const bucket = storageClient.bucket("tube-uni-podcasts");
-    const resp = await  bucket.file(fileName).exists();
-    return resp[0];
+  const bucket = storageClient.bucket("tube-uni-podcasts");
+  const resp = await bucket.file(fileName).exists();
+  return resp[0];
 }
 
 const sleep = (ms: number) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+const getAudioLongGcp = async (script: string, fileName: string) => {
+  return new Promise(async (resolve, reject) => {
+    const request = {
+      parent: `projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/global`,
+      outputGcsUri: `${GS_PATH}${fileName}`,
+      input: { text: script },
+      voice: {
+        languageCode: "en-US",
+        name: "en-US-Studio-O"
+      },
+      audioConfig: {
+        audioEncoding: "LINEAR16" as any,
+        effectsProfileId: [
+          "small-bluetooth-speaker-class-device"
+        ],
+        pitch: 0,
+        speakingRate: 1
+      }
+    };
+
+    try {
+      const response = await ttsLongClient.synthesizeLongAudio(request);
+
+      const [operation] = response;
+      if (operation && operation.name) {
+
+        let counter = 0;
+        do {
+          const checkDone = await ttsLongClient.checkSynthesizeLongAudioProgress(operation.name);
+          counter++;
+
+          if (checkDone.done) {
+            if (checkDone.error) {
+              reject(checkDone.error);
+            }
+            resolve('ok');
+            break;
+          } else if (counter >= 100) {
+            reject("Audio generation took too long");
+            break;
+          } else {
+            await sleep(1500);
+          }
+        } while (true);
+      }
+
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+const getAudioLongOpenAI = async (script: string, fileName: string) => {
+  const mp3 = await openai.audio.speech.create({
+    model: "tts-1",
+    voice: "nova",
+    input: script,
+  });
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+  storageClient.bucket("tube-uni-podcasts").file(`podcasts/${fileName}`).save(buffer);
 }
 
 export const getAudioLong = async (script: string, topic: string, duration: number) => {
-    return new Promise(async (resolve, reject) => {
-      const fileName = `${topic.replace(/ /g, "_")}_${duration}.mp3`;
-      const outputFileName = `https://storage.googleapis.com/tube-uni-podcasts/podcasts/${fileName}`;
-      const fileExists = await checkFileExists(`podcasts/${fileName}`);
-  
-      if (fileExists) {
-        resolve(outputFileName);
-      }
-  
-      const request = {
-        parent: `projects/${process.env.GOOGLE_PROJECT_NUMBER}/locations/global`,
-        outputGcsUri: `gs://tube-uni-podcasts/podcasts/${fileName}`,
-        input: {text: script},
-        voice: {
-          languageCode: "en-US",
-          name: "en-US-Studio-O"
-        },
-        audioConfig: {
-          audioEncoding: "LINEAR16" as any,
-          effectsProfileId: [
-            "small-bluetooth-speaker-class-device"
-          ],
-          pitch: 0,
-          speakingRate: 1
-        }
-      };  
-  
-      try {
-        const response = await ttsLongClient.synthesizeLongAudio(request);
-  
-        const [operation] = response;
-        if (operation && operation.name) {
-  
-          let counter = 0;
-          do {
-            const checkDone = await ttsLongClient.checkSynthesizeLongAudioProgress(operation.name);
-            counter++; 
-  
-            if (checkDone.done) {
-              if (checkDone.error) {
-                reject(checkDone.error);
-              }
-              resolve(outputFileName);
-              break;
-            } else if (counter >= 100) {
-              reject("Audio generation took too long");
-              break;
-            } else {
-              await sleep(1500);
-            }
-          } while (true);
-        }
-  
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-  
+  return new Promise(async (resolve, reject) => {
+
+    const fileName = `${topic.replace(/ /g, "_")}_${duration}.mp3`;
+    const outputUrl = `https://storage.googleapis.com/tube-uni-podcasts/podcasts/${fileName}`;
+    const fileExists = await checkFileExists(`podcasts/${fileName}`);
+
+    if (fileExists) {
+      resolve(outputUrl);
+    }
+
+    if (process.env.TTS_PROVIDER === "openai") {
+      await getAudioLongOpenAI(script, fileName);
+      resolve(outputUrl);
+    } else {
+      await getAudioLongGcp(script, fileName);
+      resolve(outputUrl);
+    }
+  });
+}
