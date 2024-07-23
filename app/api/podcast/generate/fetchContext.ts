c263ea6702fcc51bdc98d66a108b7368f43f8cb8
lizozom <lizka.k@gmail.com>
2024-07-23 18:12:09 +0300

    topics prompt

--- app/api/podcast/generate/fetchContext.ts
+++ app/api/podcast/generate/fetchContext.ts
@@ -3,9 +3,10 @@ import { getContentJson } from '../../helpers/getContent';
 
 export const fetchContext = async (topic: string, retry: boolean = false) => {
     const relavantWikiTopicsPrompt = `
-        Give me a list of AT MOST 3 relevant wikipedia page for the topic "${topic}". Return the response as a YAML list.
-        The list should only contain the topics, for example: ["Topic A", "Topic B", "Topic C"].
-        IMPORTANT! Return ONLY a YAML object. Don't add quotes or comments around it.
+        Give me a list of AT MOST 3 relevant wikipedia page for the topic "${topic}". 
+        Return the response as a JSON list.
+        The response should look like this:
+        ["Topic A", "Topic B", "Topic C"].
     `;
 
     const context: Array<string> = [];
