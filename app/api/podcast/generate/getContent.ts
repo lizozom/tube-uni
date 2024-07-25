f8c982364e1418c3178aada2940cf48ce66a73f5
lizozom <lizka.k@gmail.com>
2024-07-25 16:40:47 +0300

    test

--- app/api/podcast/generate/getContent.ts
+++ app/api/podcast/generate/getContent.ts
@@ -5,7 +5,7 @@ import OpenAI from "openai";
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY || '',
 });
 
 export interface ContentOptions {
