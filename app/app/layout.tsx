c8674519be8151ad3384fbc3495810a9ce4a93bc
lizozom <lizka.k@gmail.com>
2024-07-30 13:45:40 +0300

    fix negative margin

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -4,7 +4,7 @@ export default function AppLayout ({
   children: React.ReactNode
 }) {
   return (
-      <div className="w-100 real-100vh w-full max-w-full">
+      <div className="real-100vh w-full max-w-full">
         <div className="flex flex-col gap-4">
             <div className="flex flex-col flex-wrap content-end text-4xl px-4 pt-8 text-color-main">
                 tube uni
